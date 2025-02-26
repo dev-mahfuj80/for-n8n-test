5c52a4523cac7cc0d5503d744c4df111a9d29f8d
Md Mahfujur Rahman <mahfuj@nexstack.sg>
2025-02-26 18:55:32 +0600

    Update page.tsx

--- fe/app/page.tsx
+++ fe/app/page.tsx
@@ -25,7 +25,7 @@ Hello hoW are you
           </li>
           <li>Save and see your changes instantly. sdfsd </li>
         </ol>
-
+        <button>Click Here</button>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
