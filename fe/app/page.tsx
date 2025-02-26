218374982f6cfaca94e7e0ed4e8c745c3f093c66
Md Mahfujur Rahman <mahfuj@nexstack.sg>
2025-02-26 19:02:07 +0600

    Update page.tsx

--- fe/app/page.tsx
+++ fe/app/page.tsx
@@ -15,9 +15,9 @@ export default function Home() {
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2"> 
 
-            Get started by editing Okay by by
+            Get started by editing Okay by bysssss 
 
-Hello hoW are you
+Hello hoW are yousfsf 
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
